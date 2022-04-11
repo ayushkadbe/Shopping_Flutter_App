82ed8098b498c2f0d5d63e94ef11f71023ef0835
Ayush Kadbe <67881141+AyushKadbe@users.noreply.github.com>
2022-04-11 23:44:34 +0530

    Main.dart file for Row showing Children Elements

---
+++ main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Multiple Child Class',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //do not return scaffold as we don't need app bar etc, but our custome text on center
+    return Center(
+      child: Container(
+          alignment: Alignment.center,
+          color: Colors.white,
+          child: Row(
+            //We are using ROW to align the CHILDREN Elements in ROW
+            children: [
+              Expanded(
+                child: Text('Ayush', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text(
+                  'John',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                  child: FittedBox(
+                fit: BoxFit.contain,
+                child: const FlutterLogo(),
+              ))
+            ],
+          )),
+    );
+  }
+}
